ef8b4617ded6081b57646a37c083895beace5263
Jaydon Goodrich <jaydongoodrich@gmail.com>
2020-11-03 23:07:46 -0700

    MyFirst Component

---
+++ test/src/components/MyFirst.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>My First Component</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MyFirst'
+}
+</script>
+
+<style scoped>
+h1{
+    color: blue;
+}
+</style>
